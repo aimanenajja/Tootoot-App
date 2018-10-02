c397cd029438f6a8ea55ac332a7825ee9fba9ecb
Aïmane Najja <aimanenajja@macbook-air-van-aimane.local>
2018-10-02 14:58:10 +0200

    Initial Commit

---
+++ Tootoot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tootoot
+//
+//  Created by Aïmane Najja on 2/10/18.
+//  Copyright © 2018 Aïmane Najja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
