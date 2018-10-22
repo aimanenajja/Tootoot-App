c3c3961a30a13ac830f88350e136ab7e76402d42
derviskiratli <dervis.kiratli@hotmail.com>
2018-10-23 00:51:03 +0200

    Added the currentRide and offerRide view and controllers

---
+++ Tootoot/CurrentRideController.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentRideController.swift
+//  Tootoot
+//
+//  Created by dervis kiratli on 22/10/2018.
+//  Copyright © 2018 Aïmane Najja. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRideController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tootoot/OfferRideController.swift
@@ -0,0 +1,32 @@
+//
+//  OfferRideController.swift
+//  Tootoot
+//
+//  Created by dervis kiratli on 22/10/2018.
+//  Copyright © 2018 Aïmane Najja. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class OfferRideController: UIViewController {
+    @IBOutlet weak var carTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
